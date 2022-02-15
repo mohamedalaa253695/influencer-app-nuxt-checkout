a4db7534dddad2d3c19ecac842ac8cb6d388b72f
mohamedalaa253695 <mohamedalaa253695@gmail.com>
2022-02-15 22:33:15 +0200

    stripe payment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,50 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  ssr: true,
   head: {
     title: 'nuxt-checkout',
-    htmlAttrs: {
-      lang: 'en'
-    },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
+  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+
+    ['nuxt-stripe-module', {
+      publishableKey: 'pk_test_PEJn3x66neMVsVQBkdvdm4LF00JSCn9Hdm',
+    }],
   ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    BASE_URL: 'http://localhost:8080/api/checkout'
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {}
 }
---
+++ pages/Error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="py-5 text-center">
+      <h2>Error</h2>
+      <p class="lead">Couldn't proccess payment</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
